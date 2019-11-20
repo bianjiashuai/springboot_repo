e7711c9150d1e41bcee26a75028981b3f76a0da5
bianjiashuai <393252841@qq.com>
2019-11-20 21:40:20 +0800

    增加获取注册中心所有服务信息的功能

--- springboot_oauth2/springboot-oauth2-cus-1.x/service-demo/src/main/java/com/bjs/oauth2/controller/DemoController.java
+++ springboot_oauth2/springboot-oauth2-cus-1.x/service-demo/src/main/java/com/bjs/oauth2/controller/DemoController.java
@@ -1,16 +1,21 @@
 package com.bjs.oauth2.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.appinfo.LeaseInfo;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClient;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
+import java.util.List;
 
 @RestController
 public class DemoController {
@@ -29,4 +34,45 @@ public class DemoController {
         return "hello you!";
     }
 
+    @Autowired
+    DiscoveryClient client;
+
+    /**
+     * 获取所有服务的实例信息
+     * @return
+     */
+    @RequestMapping(value = "service.instance.info.all", method = RequestMethod.GET)
+    public Object getServiceInstanceInfo() {
+        JSONArray msl = new JSONArray();
+        List<String> services = client.getServices();
+        for (String service : services) {
+            List<ServiceInstance> sis = client.getInstances(service);
+            JSONObject obj = new JSONObject();
+            obj.put("name", service);
+            obj.put("size", sis.size());
+            boolean enable = false;
+            JSONArray ins = new JSONArray();
+            for (ServiceInstance s : sis) {
+                EurekaDiscoveryClient.EurekaServiceInstance instance = (EurekaDiscoveryClient.EurekaServiceInstance) s;
+                JSONObject sJson = new JSONObject();
+                InstanceInfo instanceInfo = instance.getInstanceInfo();
+                sJson.put("host", instance.getHost());
+                sJson.put("port", instance.getPort());
+                InstanceInfo.InstanceStatus status = instanceInfo.getStatus();
+                if (!enable && InstanceInfo.InstanceStatus.UP.equals(status)) {
+                    enable = true;
+                }
+                sJson.put("status", status);
+                LeaseInfo leaseInfo = instanceInfo.getLeaseInfo();
+                sJson.put("registrationTime", leaseInfo.getRegistrationTimestamp());
+                sJson.put("renewalTime", leaseInfo.getRenewalTimestamp());
+                sJson.put("serviceUpTime", leaseInfo.getServiceUpTimestamp());
+                ins.add(sJson);
+            }
+            obj.put("details", ins);
+            obj.put("enable", enable ? 1 : 0);
+            msl.add(obj);
+        }
+        return msl;
+    }
 }
